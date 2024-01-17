0adc6303eaa0ba0b244bb65ea587e708ab9e9d48
zhymacbookair <782738811@qq.com>
2024-01-17 17:15:29 +0800

    添加新的代码

--- openBrowsers.sh
+++ openBrowsers.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # 打开新的终端并执行命令
-osascript -e 'tell application "Terminal" to do script "echo -n -e \"\\033]0;9228download\\007\" && /Applications/QQBrowser.app/Contents/MacOS/QQBrowser -user-data-dir=\"/Users/zhymacbookair/IdeaProjects/download_browsers/9228download\" --new-window --load-extension=/Users/zhymacbookair/IdeaProjects/download-videos-from-youtube https://www.youtube.com/feed/subscriptions?purpose=9228"'
+osascript -e 'tell application "Terminal" to do script "echo -n -e \"\\033]0;9222download\\007\" && /Applications/QQBrowser.app/Contents/MacOS/QQBrowser -user-data-dir=\"/Users/zhymacbookair/IdeaProjects/download_browsers/9222download\" --new-window --load-extension=/Users/zhymacbookair/IdeaProjects/download-videos-from-youtube https://localhost:9222/downloadyoutube/bili/百年答辩"'
+osascript -e 'tell application "Terminal" to do script "echo -n -e \"\\033]0;9228download\\007\" && /Applications/QQBrowser.app/Contents/MacOS/QQBrowser -user-data-dir=\"/Users/zhymacbookair/IdeaProjects/download_browsers/9228download\" --new-window --load-extension=/Users/zhymacbookair/IdeaProjects/download-videos-from-youtube https://localhost:9228/downloadyoutube/xigw/云卷花开"'
